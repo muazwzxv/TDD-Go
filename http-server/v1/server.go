9f34d8e5d52d4b7befb75ba3d4917a54b1bf8c8d
Muaz Wazir <muazwazir@gmail.com>
2021-03-23 02:09:08 +0800

    refactor

--- http-server/v1/server.go
+++ http-server/v1/server.go
@@ -16,18 +16,22 @@ type PlayerServer struct {
 
 func (p *PlayerServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	player := strings.TrimPrefix(r.URL.Path, "/players/")
-	w.WriteHeader(http.StatusNotFound)
-	fmt.Fprint(w, p.store.GetPlayerScore(player))
+	score := p.store.GetPlayerScore(player)
+
+	if score == 0 {
+		w.WriteHeader(http.StatusNotFound)
+	}
+	fmt.Fprint(w, score)
 }
 
-func GetPlayerScore(name string) string {
+func GetPlayerScore(name string) int {
 	if name == "Pepper" {
-		return "20"
+		return 20
 	}
 
 	if name == "Floyd" {
-		return "10"
+		return 20
 	}
 
-	return ""
+	return 0
 }
